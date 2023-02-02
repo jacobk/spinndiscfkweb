e4b6677159a2557abf7fbd3c01e68f599802101a
Jacob Kristhammar <jacob.kristhammar@sinch.com>
2023-02-02 20:51:00 +0100

    Change index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,17 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import logo from "@site/static/img/logo.png";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx("hero", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+        <img src={logo} style={{ width: 300 }} />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           {/* <Link
